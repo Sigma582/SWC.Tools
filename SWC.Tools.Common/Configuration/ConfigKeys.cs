800d1bb34db76c7e5a8256341459d406d1a12e5a
Sigma582 <elred13@gmail.com>
2017-08-01 13:57:18 +0100

    config keys

---
+++ SWC.Tools.Common/Configuration/ConfigKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWC.Tools.Common.Configuration
+{
+    public static class ConfigKeys
+    {
+        public const string AndroidServerUrl = "androidServerUrl";
+        public const string LastPlayerId = "lastPlayerId";
+        public const string LastPlayerSecret = "lastPlayerSecret";
+    }
+}
--- SWC.Tools.TestConsole/Program.cs
+++ SWC.Tools.TestConsole/Program.cs
@@ -2,6 +2,7 @@
 using SWC.Tools.Common.Layout;
 using SWC.Tools.Common.Networking;
 using SWC.Tools.Common.Networking.Json.CommandArgs;
+using SWC.Tools.Common.Configuration;
 
 namespace SWC.Tools.TestConsole
 {
@@ -9,7 +10,7 @@ namespace SWC.Tools.TestConsole
     {
         public static void Main(string[] args)
         {
-
+            var mgr = new MessageManager(ConfigKeys.AndroidServerUrl);
 
         }
     }
