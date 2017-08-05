937b561dc7a23ed871bb8fe72cce8c4799e7ba9c
Sigma582 <elred13@gmail.com>
2017-08-05 15:02:31 +0100

    credentials for test console

--- SWC.Tools.TestConsole/Program.cs
+++ SWC.Tools.TestConsole/Program.cs
@@ -3,6 +3,7 @@ using SWC.Tools.Common.Layout;
 using SWC.Tools.Common.Networking;
 using SWC.Tools.Common.Networking.Json.CommandArgs;
 using SWC.Tools.Common.Configuration;
+using System.IO;
 
 namespace SWC.Tools.TestConsole
 {
@@ -10,8 +11,8 @@ namespace SWC.Tools.TestConsole
     {
         public static void Main(string[] args)
         {
-            var mgr = new MessageManager(ConfigKeys.AndroidServerUrl);
-
+            var data = File.ReadAllLines("cred.txt");
+            var mgr = new MessageManager(ConfigKeys.AndroidServerUrl, data[0], data[1]);
         }
     }
 }
