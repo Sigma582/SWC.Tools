ec8afbd746cbc1473f62d1ecf6abeb27678a66ba
Sigma582 <elred13@gmail.com>
2017-09-18 22:07:55 +0100

    test console fix

--- SWC.Tools.TestConsole/Program.cs
+++ SWC.Tools.TestConsole/Program.cs
@@ -4,6 +4,7 @@ using SWC.Tools.Common.Networking;
 using SWC.Tools.Common.Networking.Json.CommandArgs;
 using SWC.Tools.Common.Configuration;
 using System.IO;
+using System.Configuration;
 
 namespace SWC.Tools.TestConsole
 {
@@ -12,7 +13,8 @@ namespace SWC.Tools.TestConsole
         public static void Main(string[] args)
         {
             var data = File.ReadAllLines("cred.txt");
-            var mgr = new MessageManager(ConfigKeys.AndroidServerUrl, data[0], data[1]);
+            var mgr = new MessageManager(ConfigurationManager.AppSettings[ConfigKeys.AndroidServerUrl], data[0], data[1]);
+            mgr.Init();
         }
     }
 }
